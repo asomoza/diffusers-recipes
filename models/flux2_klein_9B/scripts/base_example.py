dd847800a50c420a7e267b7c669ace2ed863d335
Alvaro Somoza <somoza.alvaro@gmail.com>
2026-01-15 17:10:25 -0300

    flux2 klein 9B couple of basic examples

---
+++ models/flux2_klein_9B/scripts/base_example.py
@@ -0,0 +1,27 @@
+import os
+
+import torch
+from diffusers import Flux2KleinPipeline
+
+
+device = "cuda"
+dtype = torch.bfloat16
+
+pipe = Flux2KleinPipeline.from_pretrained("black-forest-labs/FLUX.2-klein-9B", torch_dtype=dtype)
+pipe.enable_model_cpu_offload()
+
+prompt = "photo of a capybara riding a skateboard straight down the middle of a sunny city street, shot from a low-angle, the capybara wears an obviously fake, windblown blonde wig, sunglasses, oversized skater shorts, and scuffed skate shoes, paws balanced confidently on the board; cinematic midday lighting with crisp shadows, shallow depth of field with softly blurred buildings and pedestrians in the background, subtle motion blur on the skateboard wheels; “Diffusers” bold graffiti painted on the asphalt; humorous contrast between the capybara's calm, stoic expression and the high-energy skate culture vibe."
+
+image = pipe(
+    prompt=prompt,
+    height=1024,
+    width=1024,
+    guidance_scale=1.0,
+    num_inference_steps=4,
+    generator=torch.Generator(device=device).manual_seed(42),
+).images[0]
+
+if not os.path.exists("./outputs/flux2_klein_9B"):
+    os.makedirs("./outputs/flux2_klein_9B")
+
+image.save("./outputs/flux2_klein_9B/base_example.png")
